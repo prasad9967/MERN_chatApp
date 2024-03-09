9b001a108aa354e1860f38b25e92f0df55eb39ad
prasad9967 <gubbaladurgaprasad25@gmail.com>
2024-03-09 19:35:10 +0530

    jh

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,11 +1,18 @@
-import React  from 'react'
+import React  from 'react';
+import useConversation from '../../zustand/useConversation'
 
-// 3:22
 const Conversation = ({conversation, lastIdx, emoji}) => {
- 
+
+  const {selectedConversation, setSelectedConversation} = useConversation()
+  const isSelected = selectedConversation?._id === conversation._id;
+  
   return (
     <>
-      <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+      <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+      ${isSelected ? 'bg-sky-500' : "" }
+      `}
+      onClick={()=> setSelectedConversation(conversation)}
+      >
         <div className='avatar online'>
             <div className='w-12 rounded-full'>
                 <img src={conversation.profilePic} alt='user avatar' />
