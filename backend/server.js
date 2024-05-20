cf4f75797127146e25ac68ed06294b81b33f8f38
prasad9967 <gubbaladurgaprasad25@gmail.com>
2024-05-20 13:36:25 +0530

    last Commit

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
-// app.get('/',(req,res)=>{
-//     // root route htt://localhost:5000
-//     res.send('Hi there')
-// })
+
 
 
 
