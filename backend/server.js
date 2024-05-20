6b392da5bcdba9f31712beb026db60e72396664b
prasad9967 <gubbaladurgaprasad25@gmail.com>
2024-05-20 08:43:53 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
+
 
 import connnectToMongoDB from './db/connectToMongoD.js'
 
@@ -11,6 +13,8 @@ import {app, server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json()) // to parse the incoming requests with JSON payload (req.body)
@@ -20,6 +24,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 // app.get('/',(req,res)=>{
 //     // root route htt://localhost:5000
